0d3007b219108ba3f8e2e7933d01503426daa3a1
unknown <szymonjarzab1997@gmail.com>
2020-10-25 17:43:36 +0100

    database structure update

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth.models import User
 class Floor(models.Model):
     level = models.IntegerField()
 
+    def __str__(self):
+        return str(self.level)
+
 
 class Room(models.Model):
     DESTINATION_CHOICES = [
@@ -16,9 +19,13 @@ class Room(models.Model):
     ]
     floor = models.ForeignKey(Floor, on_delete=models.DO_NOTHING)
     sign = models.CharField(max_length=100)
+    availability_level = models.PositiveIntegerField()
     projector = models.BooleanField()
-    places = models.IntegerField()
+    number_of_places = models.PositiveIntegerField()
     destination = models.CharField(max_length=20, choices=DESTINATION_CHOICES)
+
+    def __str__(self):
+        return self.sign
 
 
 class Reservation(models.Model):
@@ -31,14 +38,22 @@ class Reservation(models.Model):
         ('6', '18:00-20:00')
     ]
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    room = models.ForeignKey(Room, on_delete=models.DO_NOTHING)
 #   date_begin = models.DateTimeField()
 #   date_end = models.DateTimeField()
     date = models.DateField()
     time = models.CharField(max_length=12, choices=TIME_CHOICES)
+    is_upcoming = models.BooleanField()
+
+    def __str__(self):
+        return self.user.first_name
 
 
 class Message(models.Model):
-    reservation = models.OneToOneField(Reservation, on_delete=models.DO_NOTHING)
+    reservation = models.ForeignKey(Reservation, on_delete=models.DO_NOTHING)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     content = models.CharField(max_length=1000)
-    date = models.DateTimeField(auto_now_add=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[0:10]
