566f7f1bfc2553be0bf17f0f6f96b402b8339c9f
unknown <szymonjarzab1997@gmail.com>
2020-10-25 17:42:21 +0100

    settings update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2u1csc_b*$kwa1yqtf1&a^=noqsu)tlnizm4=tug#l*97ay7%!'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = '2u1csc_b*$kwa1yqtf1&a^=noqsu)tlnizm4=tug#l*97ay7%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
