74e8338010a10a44395a6e655b301ce88be0a755
unknown <szymonjarzab1997@gmail.com>
2020-10-26 20:08:25 +0100

    bugfix

--- backend/pages/tasks.py
+++ backend/pages/tasks.py
@@ -1,12 +1,15 @@
 from pages.models import Reservation
 import threading
 from datetime import datetime
+import pytz
 
 
 def update_reservation_status():
     res = Reservation.objects.all().filter(is_upcoming=True)
+    utc = pytz.UTC
     for reservation in res:
-        if reservation.datetime_begin < datetime.now():
+        print(str(reservation.datetime_begin))
+        if reservation.datetime_begin < utc.localize(datetime.now()):
             reservation.is_upcoming = False
             reservation.save()
     threading.Timer(60, update_reservation_status).start()
